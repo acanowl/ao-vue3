433b94b9515ce1c66bc8b837c3453389c8bb7787
徐仔 <kami.x1g@163.com>
2022-07-26 02:17:47 +0800

    🐎 ci: 新增eslint、pug相关配置

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: "vue-eslint-parser",
+  rules: {}
+}
