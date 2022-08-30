59a9f22ac5a9e1fe3b780a02babb3f130977c3a1
徐仔 <kami.x1g@163.com>
2022-08-31 01:06:58 +0800

    ✨ feat: 新增pinia

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import router from '@/router'
 import App from './App.vue'
 import '@/assets/css/index.scss'
 
+import { createPinia } from 'pinia'
+import piniaPluginPresist from 'pinia-plugin-persist'
+const pinia = createPinia()
+pinia.use(piniaPluginPresist)
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(ElementPlus)
+app.use(ElementPlus).use(pinia)
 app.use(router).mount('#app')
